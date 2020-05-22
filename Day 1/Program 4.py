cc707bbc29a1df1a3c720c98abeeaa6d0d2cd712
ankit595 <56728798+ankit595@users.noreply.github.com>
2020-05-22 10:21:02 +0530

    Create Program 4.py

---
+++ Day 1/Program 4.py
@@ -0,0 +1,6 @@
+cost_price=int(input("Enter cost price:-"))
+sell_price=int(input("Enter selling price:-"))
+profit=sell_price-cost_price
+sell_price=sell_price+profit*.05
+print(profit)
+print(sell_price)
