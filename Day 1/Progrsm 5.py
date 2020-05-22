b92ad22ee413cf945d15468dd086db6d983f8a09
ankit595 <56728798+ankit595@users.noreply.github.com>
2020-05-22 10:59:47 +0530

    Create Progrsm 5.py

---
+++ Day 1/Progrsm 5.py
@@ -0,0 +1,15 @@
+run_p1=int(input("Enter the run scored of Player 1 in 60 balls :"))
+run_p2=int(input("Enter the run scored of Player 2 in 60 balls :"))
+run_p3=int(input("Enter the run scored of Player 3 in 60 balls :"))
+strikerate1=run_p1 * 100/60
+strikerate2=run_p2 * 100/60
+strikerate3=run_p3 * 100/60
+print("Strike rate of Player 1 :",strikerate1)
+print("Strike rate of Player 2 :",strikerate2)
+print("Strike rate of Player 3 :",strikerate3)
+print("Run scored by Player 1 in more 60 balls :",run_p1 *2)
+print("Run scored by Player 2 in more 60 balls :",run_p2 *2)
+print("Run scored by Player 3 in more 60 balls :",run_p3 *2)
+print("Maximum sixes hitted by Player 1 :",run_p1//6)
+print("Maximum sixes hitted by Player 2 :",run_p2//6)
+print("Maximum sixes hitted by Player 3 :",run_p3//6)
