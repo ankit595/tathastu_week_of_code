96b3a7912e0309456109d60abbccd75dba5b36f3
ankit595 <56728798+ankit595@users.noreply.github.com>
2020-06-01 07:43:39 +0530

    Update Program 2.py

--- Day 2/Program 2.py
+++ Day 2/Program 2.py
@@ -1,16 +1,11 @@
-term = int(input("How many terms? "))
-n1, n2 = 0, 1
-count = 0
-if term <= 0:
-   print("Please enter a positive integer")
-elif term == 1:
-   print("Fibonacci sequence upto",term,":")
-   print(n1)
-else:
-   print("Fibonacci sequence:")
-   while count < term:
-       print(n1)
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+n=int(input('How many terms: '))
+x=0
+y=1
+print(x,end='')   
+sum=0
+while n-1>0:    #As we printed a number before, so we use (n-1) 
+    x=y
+    y=sum
+    sum=x+y
+    n-=1
+    print(' ',sum,end='')
